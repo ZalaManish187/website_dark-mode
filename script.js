5982d35ff8a32e08cf6f847e78695b35163c22e4
Manish Zala <zalamanish187@gmail.com>
2025-08-26 14:15:07 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 var icon = document.getElementById("icon");
+var icons = document.getElementById("icons");
 
-icon.onclick = function(){
-    document.body.classList.toggle("dark-theme");
+document.getElementById("darkModeBtn").addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
 
-    if(document.body.classList.contains("dark-theme")){
-        icon.src = "dark theme icon/sun.png";
-    }else{
-        icon.src = "dark theme icon/moon.png";
-    }
-}
+  if (document.body.classList.contains("dark-theme")) {
+    icons.src = "dark theme icon/sun.png";
+  } else {
+    icons.src = "dark theme icon/moon.png";
+  }
+});
+
+document.getElementById("resBtn").addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "dark theme icon/sun.png";
+  } else {
+    icon.src = "dark theme icon/moon.png";
+  }
+});
